36481cebeced6d8aa6424c32b5bde4c0ff232ebb
Tim Lisemer <tim.lisemer@gmx.de>
2021-12-20 16:42:32 +0100

    Navigation Bar

--- dev/login.php
+++ dev/login.php
@@ -1,26 +1,12 @@
 <html>
  <head>
-    <link rel="stylesheet" type="text/css" href="../css/style.css">
-    <link rel="shortcut icon" href="/images/yakweide_logo_klein.png" />
   <title>YakWeide.de</title>
  </head>
  <body>
    
 <nav>
-   <div id="left">
-      <img src="/images/yakweide_logo_klein.png" alt="Logo" height="50">
-    </div>
-
-
-   
-    <div id="right">
-      <button onclick="window.location.href = 'login.php';">Login/Register</button>
-      <button onclick="window.location.href = 'account.php';">Account</button>
-   </div>
-
-
-
-   <div id="mid">
+    <h1>Yakweide</h1>
+    <span>
       <button onclick="window.location.href = 'https://yakweide.de/mail';">Mail</button>
       <button onclick="window.location.href = 'https://discord.yakweide.de/';">Discord Server</button>
       <button onclick="window.location.href = 'https://steam.yakweide.de/';">Steam Gruppe</button>
@@ -28,16 +14,20 @@
       <button onclick="window.location.href = 'https://Thomas.yakweide.de/';">Thomas</button>
       <button onclick="window.location.href = 'https://Tonk.yakweide.de/';">Tonk</button>
       <button onclick="window.location.href = 'https://github.yakweide.de/';">Github</button>
-   </div>
+   </span>
+   <span>
+      <button onclick="window.location.href = 'account';">Account</button>
+      <button onclick="window.location.href = 'login';">Login/Register</button>
+   </span>
 </nav>
 
-
-
-
-
 <section>
-    <h2>Yakweide</h2>
-    test
+    <h2>Login</h2>
+    <form action="authorization" method="post">
+        <input type="text" name="username" placeholder="Username" required>
+        <input type="password" name="password" placeholder="Password" required>
+        <input type="submit" value="Login">
+    </form>
 </section>
 
  </body>
