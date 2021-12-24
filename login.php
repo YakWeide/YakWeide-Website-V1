02d6eb646305360830a8adeb335b3a9b7f82442d
Tim Lisemer <tim.lisemer@gmx.de>
2021-12-24 16:00:43 +0100

    DotEnv

--- login.php
+++ login.php
@@ -18,11 +18,12 @@
 
 <?php
   
-  //if (isset($_POST['username']) && isset($_POST['password'])){
+  if (isset($_POST['username']) && isset($_POST['password'])){
     
     //echo $authorization->checkUsernameAvailable($_POST['username']);
+    echo "test";
 
-  //}
+  }
 
 ?>
 
